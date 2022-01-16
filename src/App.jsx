233351a96fed9e9c637ba68d1576ebd0d975f738
Dovilė <dovile.keparute@gmail.com>
2022-01-16 21:19:56 +0200

    meme generator

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./Components/Header";
+function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header-image" src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png" alt="troll face" />
+            <h2 className="header-title">Meme generator</h2>
+            <h4 className="header-project">React Course - Project</h4>
+        </header>
+    );
+}
